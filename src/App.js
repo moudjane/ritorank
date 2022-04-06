c1c4569dd8e31dce8b8d8384d9c989d81b20e6ea
moudjane <mathis.oudjane@gmail.com>
2022-04-06 17:16:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Lol from './components/Lol'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Lol/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Lol.js
@@ -0,0 +1,17 @@
+let summonerName = "Moudjanitosaurus";
+let apiKey = process.env.REACT_APP_API_KEY;
+const Lol = () => {
+        fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`)
+
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result.name, result.profileIconId)
+                },
+                (error) => {
+        console.log(error);
+                }
+            )
+    };
+
+export default Lol;
